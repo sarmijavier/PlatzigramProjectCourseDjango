57fdd4bc220c3d3388016150cb2b60b7932c3334
sarmijavier <hjsarmiento1999@gmail.com>
2020-08-06 00:42:32 -0500

    templates

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -5,7 +5,6 @@ from django.urls import path
 from platzigram import views as local_views
 from posts import views as posts_views
 
-
 urlpatterns = [
     path('hello-world/', local_views.hello_world),
     path('sorted/', local_views.sort_integers),
--- platzigram/posts/views.py
+++ platzigram/posts/views.py
@@ -1,43 +1,43 @@
 """ Post views """
 
 #Django 
-from django.http import HttpResponse
+from django.shortcuts import render
 
 #utilities
 from datetime import datetime
 
 posts = [
     {
-        'name': 'Mont Blac',
-        'user': 'Yesica Cortés',
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U'
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name': 'Via Láctea',
-        'user': 'C. Vander',
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=903',
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
-        'name': 'Nuevo auditorio',
-        'user': 'Thespianartist',
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'https://picsum.photos/200/200/?image=1076',
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
 
 def list_posts(request):
     """ List existing posts. """
-    content = []
-    for post in posts:
-        content.append("""
-            <p><strong>{name}</strong></p>
-            <p><small>{user}- <i>{timestamp}</i></small></p>
-            <figure>
-                <img src="{picture}"/>
-            </figure>
-        """.format(**post))
 
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {'posts': posts})
