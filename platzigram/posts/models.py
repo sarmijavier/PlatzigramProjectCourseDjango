48c35c51d226a5b3c94389103f9b20b6f09759ef
sarmijavier <hjsarmiento1999@gmail.com>
2020-08-09 01:19:17 -0500

    ORM and MTV

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -1,3 +1,27 @@
+""" Posts models."""
+
+#Django
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """ User model."""
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    is_admin = models.BooleanField(default=False)
+
+    bio = models.TextField(blank=True)
+
+    birth_date = models.DateField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """ Return email """
+        return self.email
