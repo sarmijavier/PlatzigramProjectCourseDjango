34bd351a1f1d0249ae6bc5339abfc2af983cbed8
Yahuarcani <mayayahuarcani@gmail.com>
2020-12-07 15:51:40 -0500

    Create Users manually

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -1,3 +1,30 @@
+""" Posts Models """
+
+#Django
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """ User Model """
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    is_admin = models.BooleanField(default=False)
+
+    bio = models.TextField(blank=True)
+
+    birthdate = models.DateField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    country = models.CharField(max_length=100, null=True)
+    city = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        """ Return Email """
+        return self.email
