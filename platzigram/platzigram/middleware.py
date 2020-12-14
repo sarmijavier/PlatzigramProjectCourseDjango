0d56e467f79b4e3d2ed824c4080438af29712a88
Yahuarcani <mayayahuarcani@gmail.com>
2020-12-13 23:57:53 -0500

    middleware

--- platzigram/platzigram/middleware.py
+++ platzigram/platzigram/middleware.py
@@ -1,5 +1,11 @@
 """ Platzigram middleware catalog. """
 
+
+#Django
+from django.shortcuts import redirect
+from django.urls import reverse
+
+
 class ProfileCompletionMiddleware:
     """ Profile completion middleware.
         Ensure every user that is interacting with the platform
@@ -9,3 +15,16 @@ class ProfileCompletionMiddleware:
     def __init__(self, get_response):
         """ Middleware initialization. """
         self.get_response = get_response
+    
+
+    def __call__(self, request):
+        """ Code to be executed for each request before the view is called """
+        if not request.user.is_anonymous:
+            profile = request.user.profile 
+
+            if not profile.picture or not profile.biography:
+                if request.path != reverse('update_profile') and request.path != reverse('logout'):
+                    return redirect('update_profile')
+        
+        response = self.get_response(request)
+        return response
